02fbcfc139cd5771c87c00763f5377d3f47cd610
AMelville06 <anthonymelville.ny283@outlook.com>
2025-04-12 17:55:36 -0400

    reset email fix? 2

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const signUpButton=document.getElementById('signUpButton');
 const signInButton=document.getElementById('signInButton');
 const signInForm=document.getElementById('signIn');
 const signUpForm=document.getElementById('signup');
+const recoverForm=document.getElementById('reset');
 
 signUpButton.addEventListener('click',function(){
     signInForm.style.display="none";
