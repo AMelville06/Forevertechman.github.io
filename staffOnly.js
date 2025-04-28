a93c2ded1db429bcf1ed22edfa1f3b875b894b2a
AMelville06 <anthonymelville.ny283@outlook.com>
2025-04-27 20:21:19 -0400

    WORK

--- staffOnly.js
+++ staffOnly.js
@@ -1,7 +1,7 @@
+// staffOnly.js
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
-import {auth, db} from './firebaseauth.js';
-import staffUserIds from './staff_ids.js';
+import { auth, db } from './firebaseauth.js';
 
 async function isUserStaff(user) {
     if (!user) {
@@ -23,17 +23,52 @@ async function checkStaffStatusAndRedirect() {
     const user = auth.currentUser;
 
     if (!user) {
-        window.location.href = "homepage.html";
+        window.location.href = "index.html";
         return;
     }
 
     const isStaff = await isUserStaff(user);
 
     if (!isStaff) {
-        window.location.href = "homepage.html";
+        window.location.href = "index.html";
+    }
+}
+
+async function updateUsernameDisplay(user) {
+    if (user) {
+        console.log("User is logged in:", user);
+        const docRef = doc(db, "users", user.uid);
+        try {
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                const userData = docSnap.data();
+                document.getElementById('usernameTopRight').innerText = `Hello, ${userData.firstName}`;
+            } else {
+                console.log("No such document!");
+                document.getElementById('usernameTopRight').innerText = "Hello, User"; // Default value
+            }
+        } catch (error) {
+            console.error("Error getting document:", error);
+            document.getElementById('usernameTopRight').innerText = "Hello, User"; // Default value
+        }
+    } else {
+        console.log("User is not logged in");
+        document.getElementById('usernameTopRight').innerText = "Hello, User"; // Default value
     }
 }
 
 onAuthStateChanged(auth, async (user) => {
     await checkStaffStatusAndRedirect();
+    await updateUsernameDisplay(user);
+});
+
+
+// Handle logout
+document.getElementById('logout').addEventListener('click', () => {
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth).then(() => {
+        window.location.href = 'homepage.html';
+    }).catch((error) => {
+        console.error("Error signing out:", error);
+    });
 });
