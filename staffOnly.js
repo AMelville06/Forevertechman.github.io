d5f94d284d1c71f51e3cefaa3128a7a9c5a8bd63
AMelville06 <anthonymelville.ny283@outlook.com>
2025-04-27 20:27:23 -0400

    Update staffOnly.js

--- staffOnly.js
+++ staffOnly.js
@@ -1,8 +1,22 @@
 // staffOnly.js
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
-import { auth, db } from './firebaseauth.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 
+// Your Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyA8eNKm8fBaUcKJy0jozX4U_0VqCHS2WvQ",
+    authDomain: "forevertechman-website.firebaseapp.com",
+    projectId: "forevertechman-website",
+    storageBucket: "forevertechman-website.appspot.com",
+    messagingSenderId: "318552507372",
+    appId: "1:318552507372:web:75bf8c8e432157e32736c9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
 async function isUserStaff(user) {
     if (!user) {
         return false;
